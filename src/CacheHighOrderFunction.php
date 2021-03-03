51e8db057a7f001bb2c3e93fc37fcb042f7be7d0
Juan Pablo Barreto <juampi92@gmail.com>
2021-03-03 19:20:55 +0100

    CacheHighOrderFunction & ServiceProvider

---
+++ src/CacheHighOrderFunction.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Juampi92\LaravelQueryCache;
+
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Support\Facades\Cache;
+
+class CacheHighOrderFunction
+{
+    private string $key;
+
+    /** @var \DateInterval|\DateTimeInterface|int|null */
+    private $ttl;
+
+    private $query;
+
+    /**
+     * CachedQueryBuilder constructor.
+     * @param string $key
+     * @param \DateTimeInterface|\DateInterval|int|null $ttl
+     * @param \Illuminate\Database\Query\Builder|\Illuminate\Database\Eloquent\Builder $query
+     */
+    public function __construct(string $key, $ttl, $query)
+    {
+        $this->key = $key;
+        $this->ttl = $ttl;
+        $this->query = $query;
+    }
+
+    public function __call($method, $arguments)
+    {
+        if (!$this->ttl) {
+            return Cache::rememberForever(
+                $this->key,
+                fn () => $this->callQuery($method, $arguments)
+            );
+        }
+
+        return Cache::remember(
+            $this->key,
+            $this->ttl,
+            fn () => $this->callQuery($method, $arguments)
+        );
+    }
+
+    private function callQuery($method, $arguments)
+    {
+        $result = $this->query->$method(...$arguments);
+
+        if ($result instanceof EloquentBuilder || $result instanceof QueryBuilder) {
+            throw new \RuntimeException('You cant cache a query. Remember to use ->cache() right before executing the query');
+        }
+
+        return $result;
+    }
+}
---
+++ src/LaravelQueryCacheServiceProvider.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Juampi92\LaravelQueryCache;
+
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Support\ServiceProvider;
+
+class LaravelQueryCacheServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->registerBaseMacro();
+
+        foreach ($this->getCustomTimes() as $macroName => $ttl) {
+            $this->registerCustomMacro($ttl, $macroName);
+        }
+    }
+
+    protected function getCustomTimes(): array
+    {
+        return [
+            'cacheMinute' => 60,
+            'cacheHour' => 60 * 60,
+            'cacheDay' => 60 * 60 * 24,
+            'cacheWeek' => 60 * 60 * 24 * 7,
+        ];
+    }
+
+    private function registerBaseMacro(): void
+    {
+        QueryBuilder::macro('cache', function ($name, $ttl) {
+            /** @var QueryBuilder $this */
+            return new CacheHighOrderFunction($name, $ttl, $this);
+        });
+        EloquentBuilder::macro('cache', function ($name, $ttl) {
+            /** @var EloquentBuilder $this */
+            return new CacheHighOrderFunction($name, $ttl, $this);
+        });
+    }
+
+    /**
+     * Will register every custom
+     * @param  string  $macroName
+     * @param $ttl
+     */
+    private function registerCustomMacro(string $macroName, $ttl): void
+    {
+        QueryBuilder::macro($macroName, function ($name) use ($ttl) {
+            /** @var QueryBuilder $this */
+            return new CacheHighOrderFunction($name, $ttl, $this);
+        });
+        EloquentBuilder::macro($macroName, function ($name) use ($ttl) {
+            /** @var EloquentBuilder $this */
+            return new CacheHighOrderFunction($name, $ttl, $this);
+        });
+    }
+}
